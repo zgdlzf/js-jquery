70e4dc40aa34a8b7adb1b716317c03a634df7a5e
zgdlzf <1105893943@qq.com>
2017-06-07 14:29:20 +0800

    js特效1day

---
+++ myJS.js
@@ -0,0 +1,13 @@
+$(function(){
+    // $("#btn").click(function(){
+    //     alert("hello world!");
+    // })
+    // $("p:odd").css("color","red");
+    // $("p:even").css("color","red");
+    // $("p:first").css("color","red");
+    // $("p:last").css("color","red");
+    // $("p:first-child").css("color","red");
+    // $("p.last-child").css("color","red");
+    $("p:nth-child(2)").css("color","red");
+    // $("p:nth-of-type(1)").css("color","red");
+})
