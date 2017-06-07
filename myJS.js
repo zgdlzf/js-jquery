56a0e43a74ad133d0ead6a269f0f2489737540c2
zgdlzf <1105893943@qq.com>
2017-06-07 17:55:04 +0800

    js2

--- myJS.js
+++ myJS.js
@@ -1,13 +1,87 @@
 $(function(){
+    /**
+     * 引入jquery
+     */
     // $("#btn").click(function(){
     //     alert("hello world!");
     // })
+    /**
+     * jquery 选择器
+     */
     // $("p:odd").css("color","red");
     // $("p:even").css("color","red");
     // $("p:first").css("color","red");
     // $("p:last").css("color","red");
     // $("p:first-child").css("color","red");
     // $("p.last-child").css("color","red");
-    $("p:nth-child(2)").css("color","red");
+    // $("p:nth-child(2)").css("color","red");
     // $("p:nth-of-type(1)").css("color","red");
+    // $("a[title").css("color","red");
+    // $("a[href^=www]").css("color","red");
+    // $("a[href$=pdf]").css("color","red");
+    // $("a[href='www.adsa.doc").css("color","red");
+    // $("a[href*=pdf]").css("color","red");
+    // $("input[type=button").css("color","red");
+    // $(":button").attr("disabled","disabled");
+    // $("input[disabled=disabled]").css("width","200px");
+    // $(":disabled").css("width","200px");
+    // $(":enable")
+    // $(":text")
+    // $(":file")
+    // $(":selected")
+    // $(":visible")
+    // $(":submit")
+    // $("div").eq(0).css("color","red");
+    // $("div:eq(1)").css("color","red");
+    // $("span:gt(2)").css("color","red");
+    // $("span").eq(2).siblings().css("color","red");
+    // $("span").not(":eq(2)").css("color","red");
+    // $("input").not(":button")
+    // $("span").filter(":eq(2)").css("color","red");
+    // $(".dl").parent().parent().css("color","red");
+    // $(".dl").children().css("color","red");
+    // $(".dl").prev().css("color","red");
+    // $(".dl").nextAll().css("color","red");
+      /**
+     * jquery size addClass removeClass find
+     */
+        // alert($("div").size());
+        // $(".cl").css({"color":"red","font-size":"30px"});
+        // $(".cl").addClass("c2 c3");
+        // $(".cl").removeClass("c2");
+        // $(".cl").find("span").css("color","red");
+    /**
+     * jquery 链
+     */
+    // $("div").addClass("c1").filter(function(index){return index==1||$("this").attr("id")==d3});
+    // $("div").find("p").addClass("c2").addClass("c3");
+    // $("div").find("p").addClass("c2").end().addClass("c1");
+    // $("div").find("p").addClass("c2").andSelf().addClass("c1");
+    /**
+     * jquery 获取属性
+     */
+    // alert($("#d1").attr("title"));
+    // $("#d1").attr("title","aaaa");
+    // $("#d1").attr({"title":"aaa","id":"d2"});
+    // alert($("#d1").html());
+    // alert($("#d1").text());
+    // alert($("#d1").hasClass("c1"));
+    // alert($("#d1").find("p").is("p"));  
+    /**
+     * jquery append insertAfter mouseover mouseout hover
+     */
+    $("#d1").append("<p>a</p>");
+    $("<p>a</p>").appendTo($("#d1"));
+    $("<p>a</p>").insertAfter($("#d1"));
+    $("#d1").mouseover(function(){
+        alert("aaa");
+    });
+    $("#d1").mouseout(function(){
+        alert("aaa");
+    });
+    $("#d1").hover(function(){
+        alert("a");
+    },function(){
+        alert("b");
+    });
 })
